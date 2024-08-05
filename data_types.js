e2068afd2a2c9c3fc1bb454d5c3ba7724d18d112
Pradip <dellpidarp57@gmail.com>
2024-08-05 07:28:08 +0545

    data types on going

---
+++ data_types.js
@@ -0,0 +1,24 @@
+//Data Types in JavaScript
+
+//a. String (enclosed in "", '', ``)
+
+var age = "32";
+
+//b. Numbers
+//-integer
+//-float
+//-bigInt = 9e5 = 9 * 10^5
+
+//c. boolean
+//true
+//false
+
+let isDone = true;
+
+//d. null
+//Different ways for representing null
+
+let salary = null; //best practice
+let phnNumber = "";
+let email = "";
+let address = ``;
